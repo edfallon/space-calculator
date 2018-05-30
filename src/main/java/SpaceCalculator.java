ea6918e99ea2bdc0f151642c2afc5f874806e861
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-30 17:13:45 +0100

    passing test for get age on earth

--- src/main/java/SpaceCalculator.java
+++ src/main/java/SpaceCalculator.java
@@ -1,2 +1,14 @@
 public class SpaceCalculator {
+
+    private double ageInSeconds;
+
+
+    public SpaceCalculator(double ageInSeconds) {
+        this.ageInSeconds = ageInSeconds;
+    }
+
+    public double ageOnEarth(){
+        double result = Math.floor(ageInSeconds / 31557600);
+        return result;
+    }
 }
