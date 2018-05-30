a8fce67e3b1cf975a22cbe1f6bb89281ac07c653
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-30 17:11:29 +0100

    test created to get earth age

--- src/test/java/SpaceCalculatorTest.java
+++ src/test/java/SpaceCalculatorTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class SpaceCalculatorTest {
+
+
+    SpaceCalculator spaceCalculator1;
+
+    @Before
+    public void setUp() throws Exception {
+        spaceCalculator1 = new SpaceCalculator(1000000000);
+    }
+
+
+
+    @Test
+    public void canGetAgeOnEarth() {
+        assertEquals(31, spaceCalculator1.ageOnEarth(), 0.01);
+    }
 }
