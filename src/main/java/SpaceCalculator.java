ff8041262c697db68bff0d90d594bd6d994dda46
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-30 17:16:15 +0100

    passing test for get age on Mercury

--- src/main/java/SpaceCalculator.java
+++ src/main/java/SpaceCalculator.java
@@ -11,4 +11,9 @@ public class SpaceCalculator {
         double result = Math.floor(ageInSeconds / 31557600);
         return result;
     }
+
+    public double ageOnMercury(){
+        double result = Math.floor(ageInSeconds / (31557600 * 0.2408467));
+        return result;
+    }
 }
